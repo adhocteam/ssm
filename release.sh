f22dcabb8e3f83018697b3a44dac61dc17f2bf43
Brian King <brian.king@adhocteam.us>
2021-01-28 00:08:42 -0500

    Put all release items in folder (#20)

--- release.sh
+++ release.sh
@@ -3,6 +3,6 @@ set -euxo pipefail
 rm -rf release
 mkdir release
 GOOS=darwin GOARCH=amd64 go build -o release/ssm-darwin-amd64
-sha256sum ssm-darwin-amd64 >release/ssm-darwin-amd64.sha
-GOOS=linux GOARCH=amd64 go build -o ssm-linux-amd64
-sha256sum ssm-darwin-amd64 >release/ssm-linux-amd64.sha
+sha256sum release/ssm-darwin-amd64 >release/ssm-darwin-amd64.sha
+GOOS=linux GOARCH=amd64 go build -o release/ssm-linux-amd64
+sha256sum release/ssm-darwin-amd64 >release/ssm-linux-amd64.sha
